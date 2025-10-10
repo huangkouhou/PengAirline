f72ec6cc2404ce62294c1bbb4473689ff2b11f2f
Peng Huang <huangkouhou@gmail.com>
2025-10-10 11:29:06 +0900

    Initial commit

---
+++ backend/src/main/java/com/peng/PengAirline/PengAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.peng.PengAirline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PengAirlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/peng/PengAirline/PengAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peng.PengAirline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengAirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
