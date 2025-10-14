7f2d721d297535c61264d02f9a440b20bd08fd72
Peng Huang <huangkouhou@gmail.com>
2025-10-14 14:50:16 +0900

    Airport Entity&DTO

---
+++ backend/src/main/java/com/peng/PengAirline/entities/Airport.java
@@ -0,0 +1,43 @@
+package com.peng.PengAirline.entities;
+
+import com.peng.PengAirline.enums.City;
+import com.peng.PengAirline.enums.Country;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "airports")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private City city;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Country country;
+
+    @Column(unique = true, nullable = false, length = 3)
+    private String iataCode;
+
+}
