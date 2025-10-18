a6a81b31fd2d60fab10c04c2c17fc1819449e535
Peng Huang <huangkouhou@gmail.com>
2025-10-18 14:05:41 +0900

    BUGFIX

--- backend/src/main/java/com/peng/PengAirline/repo/UserRepo.java
+++ backend/src/main/java/com/peng/PengAirline/repo/UserRepo.java
@@ -11,11 +11,12 @@ import com.peng.PengAirline.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Long>{
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);//根据用户的 email 字段查找用户。
 
     boolean existsByEmail(String email);
 
+    //查找所有拥有特定角色名的用户列表。
     @Query("SELECT u FROM User u JOIN u.roles r where r.name = :roleName")
-    List<User> findByName(@Param("roleName") String roleName);
+    List<User> findByRoleName(@Param("roleName") String roleName);
     
 }
