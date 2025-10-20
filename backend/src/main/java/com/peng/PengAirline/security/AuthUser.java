bf94ccc229ebfc59f06a75de8e52327e2f8f0c98
Peng Huang <huangkouhou@gmail.com>
2025-10-20 14:17:47 +0900

    Auth User

---
+++ backend/src/main/java/com/peng/PengAirline/security/AuthUser.java
@@ -0,0 +1,43 @@
+package com.peng.PengAirline.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.peng.PengAirline.entities.User;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class AuthUser implements UserDetails{
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return user.isActive();
+    }
+
+}
