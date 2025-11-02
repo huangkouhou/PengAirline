ee681267697d1cf9c09be7763080d512487774c2
Peng Huang <huangkouhou@gmail.com>
2025-11-02 10:56:41 +0900

    Flight Service Interface

--- backend/src/main/java/com/peng/PengAirline/services/FlightService.java
+++ backend/src/main/java/com/peng/PengAirline/services/FlightService.java
@@ -1,5 +1,22 @@
 package com.peng.PengAirline.services;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.peng.PengAirline.dtos.CreateFlightRequest;
+import com.peng.PengAirline.dtos.FlightDTO;
+import com.peng.PengAirline.dtos.Response;
+import com.peng.PengAirline.enums.City;
+import com.peng.PengAirline.enums.Country;
+import com.peng.PengAirline.enums.FlightStatus;
+
 public interface FlightService {
 
+    Response<?> createFlight(CreateFlightRequest CreateFlightRequest);
+    Response<FlightDTO> getFlightById(Long Id);
+    Response<List<FlightDTO>> getAllFlight();
+    Response<?> updateFlight(CreateFlightRequest createFlightRequest);
+    Response<List<FlightDTO>> searchFlight(String departureIata, String arrivalPortIata, FlightStatus status, LocalDate departureDate);
+    Response<List<City>> getAllCities();
+    Response<List<Country>> getAllCountries();
 }
