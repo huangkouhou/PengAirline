03b56b45632a4b5a1eaaf19adc1af7bf64f6ab32
Peng Huang <huangkouhou@gmail.com>
2025-11-02 10:39:45 +0900

    BUG FIX

--- backend/src/main/java/com/peng/PengAirline/controller/AirportController.java
+++ backend/src/main/java/com/peng/PengAirline/controller/AirportController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.peng.PengAirline.dtos.AirportCreateDTO;
-import com.peng.PengAirline.dtos.AirportUpdateDTO;
+import com.peng.PengAirline.dtos.AirportDTO;
 import com.peng.PengAirline.dtos.Response;
 import com.peng.PengAirline.services.AirportService;
 
@@ -38,18 +38,18 @@ public class AirportController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Response<?>> updateAirport(
             @PathVariable Long id,
-            @Valid @RequestBody AirportUpdateDTO airportUpdateDTO) {
-        return ResponseEntity.ok(airportService.updateAirport(id, airportUpdateDTO));
+            @Valid @RequestBody AirportDTO airportDTO) {
+        return ResponseEntity.ok(airportService.updateAirport(id, airportDTO));
     }
 
 
     @GetMapping // 不带参数的 GET → 获取集合资源 GET 方法只是读取，不会改变系统状态，因此不需要加 @PreAuthorize
-    public ResponseEntity<Response<List<AirportCreateDTO>>> getAllAirports() {
+    public ResponseEntity<Response<List<AirportDTO>>> getAllAirports() {
         return ResponseEntity.ok(airportService.getAllAirports());
     }
 
     @GetMapping("/{id}") // 带路径参数（/{id}） 的 GET → 获取单个资源
-    public ResponseEntity<Response<AirportCreateDTO>> getAirportById(@PathVariable Long id) {
+    public ResponseEntity<Response<AirportDTO>> getAirportById(@PathVariable Long id) {
         return ResponseEntity.ok(airportService.getAirportById(id));
     }
 
