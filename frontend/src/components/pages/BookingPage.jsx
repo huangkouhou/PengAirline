53c8e5b7f391b66db29d096f52bc7d1c96844dd7
Peng Huang <huangkouhou@gmail.com>
2025-12-09 13:28:51 +0900

    uodate BookingPage

--- frontend/src/components/pages/BookingPage.jsx
+++ frontend/src/components/pages/BookingPage.jsx
@@ -71,6 +71,85 @@ const BookingPage = () => {
         setAvailableSeats(seats);
     };
 
+    const addPassenger = () => {
+        setPassengers([
+            ...passengers,
+            {
+                firstName: "",
+                lastName: "",
+                passportNumber: "",
+                type: "ADULT",
+                seatNumber: "",
+                specialRequest: ""
+            }
+        ]);
+    };
+
+    //删除指定位置的乘客，但保留至少一位乘客
+    const removePassenger = (index) => {
+        if (passengers.length <= 1) return;
+        const updatedPassengers = [...passengers];//把当前的 passengers 数组复制一份，起名叫 updatedPassengers
+        updatedPassengers.splice(index, 1);//在副本数组中，从 index 这个位置开始，删除 1 个元素。（比如删第2个，index就是1）
+        setPassengers(updatedPassengers);
+    };
+
+    const handlePassengerChange = (index, field, value) => {
+        const updatedPassengers = [...passengers];
+        updatedPassengers[index][field] = value;//updatedPassengers[index]：先找到 第 X 位乘客 的数据对象。updatedPassengers[index]：先找到 第 X 位乘客 的数据对象。= value：把它赋值为 Z。
+        setPassengers(updatedPassengers);
+    };
+
+    const calculateTotalPrice = () => {
+        if (!flight) return 0;
+        return passengers.reduce((total, passenger) => {
+            let price = flight.basePrice;
+            if (passenger.type === "CHILD") {
+                price *= 0.75; // 25% discount for children
+            } else if (passenger.type === "INFANT") {
+                price *= 0.1; // 90% discount for infants
+            }
+            return total + price;
+        }, 0);
+    }
+
+    const handleSubmit = async(e) => {
+
+        //Validate form
+        for (let i = 0; i < passengers.length; i++) {
+            const p = passengers[i];
+            if (!p.firstName || !p.lastName || !p.passportNumber || !p.seatNumber) {
+                showError(`Please fill all required fields for Passenger ${i + 1}`);
+                return;
+            }
+        }
+
+        try {
+            const bookingData = {
+                flightId: parseInt(flightId),
+                passengers: passengers
+            };
+
+            const response = await ApiService.createBooking(bookingData);
+
+            if (response.statusCode === 200){
+                showSuccess("Flight Booking Successfully")
+                navigate(`/profile`);
+            }
+
+        } catch (error) {
+            showError(error.response?.data?.message || "Failed to book a flight");
+        }
+
+    };
+
+    if (loading) return <div className="booking-loading">Loading Flight details</div>
+    if (!flight) return <div className="booking-error">Flight Not Found</div>
+
+    return (
+        <div></div>
+    );
+
+
 
 
 }
